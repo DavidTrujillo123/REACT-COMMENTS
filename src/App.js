00820a90fae47a9cbc0583437d2ee0a8e13d7955
DAVID TRUJILLO <davot098@gmail.com>
2024-02-21 14:21:40 -0500

    Finish app around 90%. Miss some parts of comments in bold

--- src/App.js
+++ src/App.js
@@ -1,28 +1,42 @@
 import './App.css';
 import Comments from './components/Coments'
 
+const obj = [
+  {
+    name:'Emma Bostian',
+    country: 'Suesia',
+    work: 'Ingeniera de Software',
+    business: 'Spotify',
+    comment: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat deleniti dolores expedita qui architecto commodi laborum cum iusto cumque quidem illum, ex, nostrum maxime accusamus magnam, itaque at suscipit eligendi.',
+    strongcomment: 'Lorem ipsum dolor sit amet consectetur adipisicing elit',
+    img: '1',
+  },
+  {
+    name: 'Shawn  Wang',
+    country: 'Singapur',
+    work: 'Ingeniera de Software',
+    business: 'Amazon',
+    comment: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat deleniti dolores expedita qui architecto commodi laborum cum iusto cumque quidem illum, ex, nostrum maxime accusamus magnam, itaque at suscipit eligendi.',
+    img: '2'
+  }
+]
+
 function App() {
   return (
     <div className="App">
       <div className='container-main'>
         <h1>Comentarios de suscriptores</h1>
-        <Comments
-          name='Emma Bostian'
-          country='Suesia'
-          work='Ingeniera de Software'
-          business='Spotify'
-          comment='Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat deleniti dolores expedita qui architecto commodi laborum cum iusto cumque quidem illum, ex, nostrum maxime accusamus magnam, itaque at suscipit eligendi.'
-          img='1'
-        />
-
-        <Comments
-          name='Emma Bostian'
-          country='Suesia'
-          work='Ingeniera de Software'
-          business='Spotify'
-          comment='Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat deleniti dolores expedita qui architecto commodi laborum cum iusto cumque quidem illum, ex, nostrum maxime accusamus magnam, itaque at suscipit eligendi.'
-          img='1'
-        />
+        {obj.map((element, index) => (
+          <Comments
+            key={index} // Se recomienda usar una clave única para cada elemento en la lista
+            name={element.name}
+            country={element.country}
+            work={element.work}
+            business={element.business}
+            comment={element.comment}
+            img={element.img}
+          />
+        ))}
       </div>
     </div>
   );
--- src/components/Coments.jsx
+++ src/components/Coments.jsx
@@ -10,11 +10,12 @@ function Comments(props) {
         alt='Foto de perfil'
       />
       <div className='container-text-comment'>
-        {/* <p className='name-comment'>Emma Bostian en Suesia</p> */}
-        <p className='name-comment'>{props.name} en {props.country}</p>
-        {/* <p className='work-comment'>Ingeniera de Software en Spotify</p> */}
-        <p className='work-comment'>{props.work} en {props.business}</p>
-        {/* <p className='text-comment'>"Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat deleniti dolores expedita qui architecto commodi laborum cum iusto cumque quidem illum, ex, nostrum maxime accusamus magnam, itaque at suscipit eligendi."</p> */}
+        <p className='name-comment'>
+          <strong>{props.name}</strong> en {props.country}
+        </p>
+        <p className='work-comment'>
+          {props.work} en {props.business}
+        </p>
         <p className='text-comment'>"{props.comment}"</p>
       </div>
     </div>
